89873984971a95125ea800abc0de55d3794ffeb9
Ulli-H <uanklam@yahoo.de>
2020-11-27 00:37:06 +0100

    adding queries

--- sql_queries.py
+++ sql_queries.py
@@ -27,32 +27,30 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar,
                                 longitude float)\
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time time, hour int, 
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time bigint, hour int, 
                                 day int, week int, month int, year int, weekday varchar)
 """)
 
 # INSERT RECORDS
 
-#songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, 
-#                                level, song_id, artist_id, session_id, location, 
-#                                user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""")
+songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, 
+                                level, song_id, artist_id, session_id, location, 
+                                user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);""")
 
-user_table_insert = ("""
-""")
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s);""")
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s);""")
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s);""")
 
 
-time_table_insert = ("""
-""")
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);""")
 
 # FIND SONGS
 
-song_select = ("""
-""")
-
+song_select = ("""SELECT songs.song_id, artists.artist_id
+                    FROM songs JOIN artists ON songs.artist_id = artists.artist_id ;""")
+#, artists.name, songs.title, songs.duration 
 # QUERY LISTS
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
